a05509d71e8b084237e73f39cad2e6f6401eca57
Pedro M. Dominguez <84252067+xtcedro@users.noreply.github.com>
2024-12-18 02:12:15 +0000

    Update language-modal.js

--- assets/js/language-modal.js
+++ assets/js/language-modal.js
@@ -1,5 +1,4 @@
-<script>
-        function setLanguage(lang) {
+function setLanguage(lang) {
             if (lang === 'es') {
                 window.location.href = 'index_es.html'; // Redirect to Spanish version
             } else {
@@ -8,21 +7,5 @@
                 document.getElementById('main-content').classList.remove('hidden-content');
             }
         }
-</script>
- <script>
-        // Función para mostrar/ocultar el menú móvil
-        function toggleMenu() {
-            document.querySelector('.mobile-menu').classList.toggle('show');
-        }
 
-        // Función para establecer el idioma
-        function setLanguage(lang) {
-            if (lang === 'en') {
-                window.location.href = 'index.html'; // Redirige a la versión en inglés
-            } else {
-                document.getElementById('language-modal').style.display = 'none';
-                document.body.classList.remove('hidden');
-                document.getElementById('main-content').classList.remove('hidden-content');
-            }
-        }
-    </script>
+>
