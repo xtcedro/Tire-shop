aaf546bc25cadacaacc887dd647038b2eac0675b
Pedro M. Dominguez <84252067+xtcedro@users.noreply.github.com>
2024-12-18 01:41:38 +0000

    Update language-modal.js

--- assets/js/language-modal.js
+++ assets/js/language-modal.js
@@ -1,9 +1,11 @@
-function setLanguage(lang) {
-    if (lang === 'es') {
-        window.location.href = 'index_es.html'; // Redirect to Spanish version
-    } else {
-        document.getElementById('language-modal').style.display = 'none';
-        document.body.classList.remove('hidden');
-        document.getElementById('main-content').classList.remove('hidden-content');
-    }
-}
+<script>
+        function setLanguage(lang) {
+            if (lang === 'es') {
+                window.location.href = 'index_es.html'; // Redirect to Spanish version
+            } else {
+                document.getElementById('language-modal').style.display = 'none';
+                document.body.classList.remove('hidden');
+                document.getElementById('main-content').classList.remove('hidden-content');
+            }
+        }
+</script>
